e2b1fb7ae820609a71d0edff30867bf80584a871
Rev3rse <mateusgames3301@gmail.com>
2021-07-20 18:40:25 -0300

    Update message.js

--- src/database/models/message.js
+++ src/database/models/message.js
@@ -1,8 +1,5 @@
-'use strict'
+const { Schema } = require('mongoose')
 
-const mongoose = require('mongoose')
-
-const Schema = mongoose.Schema
 const schema = new Schema({
     message: {
         unique: true,
